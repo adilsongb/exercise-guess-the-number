4ba2d427400830594e5f71f608d5e4d5455d3100
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-10-15 05:43:19 -0300

    Create game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      start: false,
+      number: '',
+      revelNum: false,
+      valueInput: '',
+      hits: 0,
+      miss: 0,
+    };
+  }
+
+  randomNumber() {
+    this.setState({ start: true });
+    const numRandom = Math.floor(Math.random() * (100 - 1)) + 1;
+    this.setState({ number: numRandom });
+  }
+
+  changeInput({ target }) {
+    const value = parseInt(target.value);
+    this.setState({ valueInput: value });
+  }
+
+  checkAnswer() {
+    const { number, valueInput } = this.state;
+    if (number === valueInput) {
+      this.setState((state) => ({
+        hits: state.hits + 1,
+        revelNum: true,
+        valueInput: '',
+      }));
+    } else {
+      this.setState((state) => ({
+        miss: state.miss + 1,
+        revelNum: true,
+        valueInput: '',
+      }));
+    }
+  }
+
+  nextNumber() {
+    this.setState({ revelNum: false });
+    this.randomNumber();
+  }
+
+  render() {
+    const { start, number, revelNum, valueInput, hits, miss } = this.state;
+
+    if (start) {
+      return (
+        <div className="App">
+          <h1 className="num-secret">
+            { revelNum ? number : '?' }
+          </h1>
+          <div className="scores">
+            <div>{ `${hits} ✔️` }</div>
+            <div>{ `❌ ${miss}` }</div>
+          </div>
+          <div className="response-input">
+            <input
+              type="text"
+              value={valueInput}
+              onChange={(e) => this.changeInput(e)}
+            />
+            <button
+              disabled={ revelNum }
+              onClick={() => this.checkAnswer()}
+            >
+              Ok
+            </button>
+          </div>
+          <button
+            disabled={ !revelNum }
+            className="btn-next"
+            onClick={() => this.nextNumber()}
+          >
+            Próximo número
+          </button>
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Adivinhe o número!</h1>
+        <p>Adivinhe qual é o número aleatório de 1 a 100</p>
+        <button
+          onClick={() => this.randomNumber()}
+          className="btn-start"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Começar
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
